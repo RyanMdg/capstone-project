5b23074e13da4b62e6b4aa0bcc1aa8a7fad8d954
RyanMdg <ryandeguia.dev@gmail.com>
2023-07-06 20:47:54 +0800

    fixing navbar

--- Front-end/clientside/src/app/components/hero.js
+++ Front-end/clientside/src/app/components/hero.js
@@ -3,7 +3,15 @@ import { BsChevronCompactLeft, BsChevronCompactRight } from "react-icons/bs";
 import { RxDotFilled } from "react-icons/rx";
 
 const Carousel = () => {
-  const slides = ["/fubu.png", "/nike.png", "/adidas.png"];
+  const slides = [
+    { image: "/fubu.png", buttonLabel: "Shop Now", textOverlay: "Fubu" },
+    { image: "/nike.png", buttonLabel: "Buy Now", textOverlay: "Nike" },
+    {
+      image: "/adidas.png",
+      buttonLabel: "View Collection",
+      textOverlay: "Adidas",
+    },
+  ];
 
   const [currentIndex, setCurrentIndex] = useState(0);
 
@@ -24,17 +32,28 @@ const Carousel = () => {
   };
 
   return (
-    <div className=" h-screen   relative screen2:w-[250%] group ">
+    <div className="h-screen relative screen2:w-[250%] group">
       <div
-        style={{ backgroundImage: `url(${slides[currentIndex]})` }}
-        className="w-full h-full   bg-center bg-cover duration-500"
-      ></div>
+        style={{ backgroundImage: `url(${slides[currentIndex].image})` }}
+        className="w-full h-full bg-center bg-cover duration-500 relative"
+      >
+        <div className="absolute inset-0 flex items-center ms-10">
+          <div className=" p-4 text-white text-center">
+            <h2 className="text-xl font-semibold text-black">
+              this one stop shop
+            </h2>
+            <button className="mt-4 px-4 py-2 bg-white text-black rounded-md">
+              Shop Now
+            </button>
+          </div>
+        </div>
+      </div>
       {/* Left Arrow */}
-      <div className="hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] left-5 text-2xl  hover:bg-[#0000008b] duration-300  p-2 bg-black/20 text-white cursor-pointer">
+      <div className="hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] left-5 text-2xl hover:bg-[#0000008b] duration-300 p-2 bg-black/20 text-white cursor-pointer">
         <BsChevronCompactLeft onClick={prevSlide} size={30} />
       </div>
       {/* Right Arrow */}
-      <div className="hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] right-5 text-2xl hover:bg-[#0000008b]  duration-300 p-2 bg-black/20 text-white cursor-pointer">
+      <div className="hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] right-5 text-2xl hover:bg-[#0000008b] duration-300 p-2 bg-black/20 text-white cursor-pointer">
         <BsChevronCompactRight onClick={nextSlide} size={30} />
       </div>
       <div className="flex top-4 justify-center py-2">
