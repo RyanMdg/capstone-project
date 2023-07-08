000fae0aebc9fed2e05f469eca540c2ca047fcff
RyanMdg <ryandeguia.dev@gmail.com>
2023-07-08 15:33:41 +0800

    fixing

--- Front-end/clientside/src/app/components/thirdsec.js
+++ Front-end/clientside/src/app/components/thirdsec.js
@@ -1,15 +1,15 @@
 const thirdsec = () => {
   return (
     <div className=" grid md:grid-cols-2 screen2:w-[250%] gap-5 my-10 max-w-7xl mx-auto">
-      <div>
+      <div className="flex justify-center">
         <img src="/grid-1.jpg" alt="" />
       </div>
-      <div className=" flex flex-col justify-center gap-10">
+      <div className=" flex items-center flex-col justify-center gap-10">
         <div>
-          <img src="/grid-2.jpg" alt="" />
+          <img className=" object-cover" src="/grid-2.jpg" alt="" />
         </div>
         <div>
-          <img src="/grid-3.jpg" alt="" />
+          <img className=" object-cover" src="/grid-3.jpg" alt="" />
         </div>
       </div>
     </div>
