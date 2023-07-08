850efb3fd9349c507f9de7bf12bf352a26fbf5a4
RyanMdg <ryandeguia.dev@gmail.com>
2023-07-08 15:22:12 +0800

    fixing grids

--- Front-end/clientside/src/app/components/thirdsec.js
+++ Front-end/clientside/src/app/components/thirdsec.js
@@ -1,7 +1,17 @@
 const thirdsec = () => {
   return (
-    <div>
-      <h1>third sec</h1>
+    <div className=" grid md:grid-cols-2 screen2:w-[250%] gap-5 my-10 max-w-7xl mx-auto">
+      <div>
+        <img src="/grid-1.jpg" alt="" />
+      </div>
+      <div className=" flex flex-col justify-center gap-10">
+        <div>
+          <img src="/grid-2.jpg" alt="" />
+        </div>
+        <div>
+          <img src="/grid-3.jpg" alt="" />
+        </div>
+      </div>
     </div>
   );
 };
