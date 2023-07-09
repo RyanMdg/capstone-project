fb01e4da3b6f7a0ca555edb31f011b24f63e694d
RyanMdg <ryandeguia.dev@gmail.com>
2023-07-09 20:32:02 +0800

    fixing thirdsec

--- Front-end/clientside/src/app/components/thirdsec.js
+++ Front-end/clientside/src/app/components/thirdsec.js
@@ -1,15 +1,31 @@
 const thirdsec = () => {
   return (
-    <div className=" screen2:relative grid md:grid-cols-2 screen2:w-[250%] my-10 gap-10 max-w-7xl mx-auto">
-      <div className="flex items-center screen2:justify-center justify-end">
-        <img className=" h-[70%] " src="/grid-1.jpg" alt="" />
+    <div className=" max-w-[80rem] screen2:w-[250%] my-10 mx-auto grid screen2:gap-4 md:grid-cols-2">
+      <div className=" mx-auto screen2:ms-[8rem] ms-[10rem] w-[70%] overflow-hidden">
+        <img
+          src="/grid-1.jpg"
+          className=" hover:scale-110 hover:brightness-75 duration-500"
+          alt=""
+        />
       </div>
-      <div className=" screen2:absolute left-[7rem] top-[70rem]  flex items-start screen2:items-center flex-col justify-center gap-5">
-        <div className="flex items-end screen2:justify-center screen2:w-[70%]">
-          <img className=" h-[70%]  " src="/grid-2.jpg" alt="" />
+      <div className="flex items-center flex-col gap-7">
+        <div className="relative w-[75%] overflow-hidden">
+          <img
+            src="/grid-2.jpg"
+            className="hover:scale-110 inset-0 transition-transform hover:brightness-75 duration-500"
+            alt=""
+          />
+
+          <span className="absolute z-50 top-[5rem] left-[6.5rem] transform -translate-x-1/2 -translate-y-1/2 text-white uppercase font-bold p-2 text-[1.5rem]">
+            New arrivals <br /> up to 70% off
+          </span>
         </div>
-        <div className="flex screen2:w-[70%] screen2:justify-center">
-          <img className="mb-10 h-[70%] " src="/grid-3.jpg" alt="" />
+        <div className="w-[75%] overflow-hidden">
+          <img
+            src="/grid-3.jpg"
+            className="hover:scale-110 hover:brightness-75 duration-500"
+            alt=""
+          />
         </div>
       </div>
     </div>
